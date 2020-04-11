22c03adfd5d226e9fe3d2dedb61e19c7e44a0f3b
jsonwu <1670544286@qq.com>
2020-04-11 16:30:59 +0800

    初始化

---
+++ sprongboot-jpa/src/main/java/com/dbjpa/sprongboot/jpa/SprongbootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.dbjpa.sprongboot.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprongbootJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprongbootJpaApplication.class, args);
+    }
+
+}
---
+++ sprongboot-jpa/src/test/java/com/dbjpa/sprongboot/jpa/SprongbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbjpa.sprongboot.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprongbootJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
